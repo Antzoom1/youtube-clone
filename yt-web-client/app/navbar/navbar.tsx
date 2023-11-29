8c165203ee74738d4b79162bfc66dfd32e92f9b4
Aaron Chan <helloaaronchan@gmail.com>
2023-11-29 13:13:05 -0800

    Finished setting up database and sign in authentication

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,10 +1,28 @@
+"use client";
+
+
 import Image from 'next/image'
 import styles from './navbar.module.css'
 import Link from 'next/link'
 import SignIn from './sign-in';
 import { onAuthStateChangedHelper } from '../firebase/firebase';
+import { useEffect, useState } from 'react';
+import { onAuthStateChanged } from '@firebase/auth';
+import { User } from 'firebase/auth';
 
 export default function Navbar() {
+    // Init user State
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedHelper((user) => {
+            setUser(user);
+        });
+        // Cleanup subscription
+        return () => unsubscribe();
+
+    })
+    
     return (
         <nav className={styles.nav}>
           <Link href="/">
@@ -14,7 +32,7 @@ export default function Navbar() {
           {
              // TODO Add a upload
           }
-          <SignIn />
+          <SignIn user={user} />
         </nav>
       );
     }
