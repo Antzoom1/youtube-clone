95c217d93b236f2b7e4048941f621b7f4249a688
Aaron Chan <helloaaronchan@gmail.com>
2023-12-04 14:13:50 -0800

    finished uploading feature

--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,38 +1,39 @@
 "use client";
 
-
-import Image from 'next/image'
-import styles from './navbar.module.css'
-import Link from 'next/link'
-import SignIn from './sign-in';
-import { onAuthStateChangedHelper } from '../firebase/firebase';
-import { useEffect, useState } from 'react';
-import { onAuthStateChanged } from '@firebase/auth';
-import { User } from 'firebase/auth';
+import Image from "next/image";
+import styles from "./navbar.module.css";
+import Link from "next/link";
+import SignIn from "./sign-in";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "@firebase/auth";
+import { User } from "firebase/auth";
+import Upload from "./upload";
 
 export default function Navbar() {
-    // Init user State
-    const [user, setUser] = useState<User | null>(null);
+  // Init user State
+  const [user, setUser] = useState<User | null>(null);
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChangedHelper((user) => {
-            setUser(user);
-        });
-        // Cleanup subscription
-        return () => unsubscribe();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
+    // Cleanup subscription
+    return () => unsubscribe();
+  });
 
-    })
-    
-    return (
-        <nav className={styles.nav}>
-          <Link href="/">
-            <Image width={90} height={20}
-              src="/youtube-logo.svg" alt="YouTube Logo"/>
-          </Link>
-          {
-             // TODO Add a upload
-          }
-          <SignIn user={user} />
-        </nav>
-      );
-    }
+  return (
+    <nav className={styles.nav}>
+      <Link href="/">
+        <Image
+          width={90}
+          height={20}
+          src="/youtube-logo.svg"
+          alt="YouTube Logo"
+        />
+      </Link>
+      {user && <Upload />}
+      <SignIn user={user} />
+    </nav>
+  );
+}
